32124f033bc00dad7b7f26d405c133fbe1d5feb4
Evan Donovan <outofegypt@gmail.com>
2014-06-23 18:45:27 -0400

    Added in the certificate completion date

--- mdl_gradebook_data.php
+++ mdl_gradebook_data.php
@@ -10,6 +10,7 @@ db_set_active('nettm5_moodle');
 db_query('DELETE FROM mdl_grades_denormalized');
 // Query for courses.
 $sql = 'SELECT mdl_course.idnumber AS course_id,
+mdl_user.id as student_id,
 mdl_user.firstname,
 mdl_user.lastname,
 mdl_user.username,
@@ -31,6 +32,7 @@ $results = db_query($sql);
 $grades = array();
 while($row = db_fetch_array($results)) {
   $studentname = $row['firstname'] . ' ' . $row['lastname'];
+  $student_id = $row['student_id'];
   $course_id = $row['course_id'];
   if(!isset($grades[$studentname][$course_id])) {
     $grades[$studentname][$course_id]['firstname'] = $row['firstname'];
@@ -46,6 +48,10 @@ while($row = db_fetch_array($results)) {
       $grades[$studentname][$course_id]['eval'] = TRUE;
       $grades[$studentname][$course_id]['completiondate'] = $row['date_modified'];
     }
+    $sql = 'select certificateid, userid, timecreated from mdl_certificate_issues 
+    join mdl_certificate on mdl_certificate.id = mdl_certificate_issues.certificateid 
+    where mdl_certificate_issues.userid = %d and mdl_certificate.course = %d'
+    $grades[$studentname][$course_id]['cert_completiondate'] = db_result(db_query($sql, $student_id, $course_id));
   }
   /* if($row['eval_complete'] == 'y') {
     $grades[$studentname][$course_id]['eval'] = TRUE;
@@ -61,7 +67,8 @@ while($row = db_fetch_array($results)) {
 if(count($grades) > 0) {
   foreach($grades as $studentname => $courses) {
     foreach($courses as $course_id => $record) {
-      db_query('INSERT INTO mdl_grades_denormalized (studentname, firstname, lastname, course_id, pretest, posttest, eval, completiondate) VALUES ("%s", "%s", "%s", "%s", %d, %d, %b, %d)', array($record['studentname'], $record['firstname'], $record['lastname'], $record['course_id'], $record['pretest'], $record['posttest'], $record['eval'], $record['completiondate']));
+      db_query('INSERT INTO mdl_grades_denormalized (studentname, firstname, lastname, course_id, pretest, posttest, eval, completiondate, cert_completiondate) VALUES ("%s", "%s", "%s", "%s", %d, %d, %b, %d, %d)', 
+        array($record['studentname'], $record['firstname'], $record['lastname'], $record['course_id'], $record['pretest'], $record['posttest'], $record['eval'], $record['completiondate'], $record['cert_completiondate']));
     }
   }
 }
