2916ecc28944249ad8ec7d7a5e6b1187d3177e4f
Evan Donovan <outofegypt@gmail.com>
2014-06-24 15:41:33 -0400

    attempt at fixing by adding course numeric id

--- mdl_gradebook_data.php
+++ mdl_gradebook_data.php
@@ -10,6 +10,7 @@ db_set_active('nettm5_moodle');
 db_query('DELETE FROM mdl_grades_denormalized');
 // Query for courses.
 $sql = 'SELECT mdl_course.idnumber AS course_id,
+mdl_course.id AS course_idnum,
 mdl_user.id as student_id,
 mdl_user.firstname,
 mdl_user.lastname,
@@ -34,7 +35,6 @@ while($row = db_fetch_array($results)) {
   $studentname = $row['firstname'] . ' ' . $row['lastname'];
   $student_id = $row['student_id'];
   $course_id = $row['course_id'];
-  echo $course_id . PHP_EOL;
   if(!isset($grades[$studentname][$course_id])) {
     $grades[$studentname][$course_id]['firstname'] = $row['firstname'];
     $grades[$studentname][$course_id]['lastname'] = $row['lastname'];
@@ -52,8 +52,8 @@ while($row = db_fetch_array($results)) {
     $sql = 'select timecreated from mdl_certificate_issues 
     join mdl_certificate on mdl_certificate.id = mdl_certificate_issues.certificateid 
     where mdl_certificate_issues.userid = %d and mdl_certificate.course = %d';
-    echo sprintf($sql, $student_id, $course_id) . PHP_EOL;
-    $grades[$studentname][$course_id]['cert_completiondate'] = db_result(db_query($sql, $student_id, $course_id));
+    echo sprintf($sql, $student_id, $row['course_idnum']) . PHP_EOL;
+    $grades[$studentname][$course_id]['cert_completiondate'] = db_result(db_query($sql, $student_id, $row['course_idnum']));
     echo $grades[$studentname][$course_id]['cert_completion_date'] . PHP_EOL;
   }
   /* if($row['eval_complete'] == 'y') {
